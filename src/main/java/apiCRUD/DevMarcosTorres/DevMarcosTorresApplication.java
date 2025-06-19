74377702aa05f8ea01bf5422d649d133fa56a15d
Torres093 <marcosalejandrot583@gmail.com>
2025-06-19 14:01:25 -0600

    primer commit

---
+++ src/main/java/apiCRUD/DevMarcosTorres/DevMarcosTorresApplication.java
@@ -0,0 +1,13 @@
+package apiCRUD.DevMarcosTorres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevMarcosTorresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevMarcosTorresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apiCRUD/DevMarcosTorres/DevMarcosTorresApplicationTests.java
@@ -0,0 +1,13 @@
+package apiCRUD.DevMarcosTorres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevMarcosTorresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
